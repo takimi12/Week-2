5a4150ec0c66a97b48651cd932db12c31d033bdd
takimi12 <tomek12olech@gmail.com>
2023-09-13 17:24:36 +0200

    zadanie 6

--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -9,21 +9,24 @@ export const ProductListItemDescription = ({
 	product: { category, name, price },
 }: ProductListItemDescriptionProps) => {
 	return (
-		<div className="mt-2 flex justify-between">
-			<div>
-				<h1 className="text-sm font-semibold text-gray-700">
-					{name}
-				</h1>
-				<p className="mt-1 text-sm text-gray-500">
-					<span className="sr-only">Kategoria</span>
-					{category}
+		<>
+			<h1 className="text-sm font-semibold text-gray-700">{name}</h1>
+			<div className="mt-2 flex justify-between">
+				<div>
+					<h3 className="text-sm font-semibold text-gray-700">
+						{name}
+					</h3>
+					<p className="mt-1 text-sm text-gray-500">
+						<span className="sr-only">Kategoria</span>
+						{category}
+					</p>
+				</div>
+				<p className="text-sm font-medium text-gray-900">
+					<span className="sr-only">Cena:</span>
+
+					{formatMoney(price / 100)}
 				</p>
 			</div>
-			<p className="text-sm font-medium text-gray-900">
-				<span className="sr-only">Cena:</span>
-
-				{formatMoney(price / 100)}
-			</p>
-		</div>
+		</>
 	);
 };
